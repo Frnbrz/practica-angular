a1f0bab2b01094d3e0824229a1aafb6a3f12a128
frnbrz <frnbrz@gmail.com>
2025-03-27 00:26:11 +0100

    Add new components and styles for home, new post, error, and post detail pages; implement footer and navigation bar

---
+++ src/app/pages/new-post/new-post.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-new-post',
+  imports: [],
+  templateUrl: './new-post.component.html',
+  styleUrl: './new-post.component.scss',
+})
+export class NewPostComponent {}
